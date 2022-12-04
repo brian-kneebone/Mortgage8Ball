d67faf4e79db7651b11d85f3184a446d288a13a6
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-12-04 11:57:26 -0500

    add bulma-calendar and update readme for it and bulma dependencies

---
+++ main.js
@@ -0,0 +1,25 @@
+// Prepare the options
+const options = {
+  type: 'date',
+  dateFormat: 'yyyy-MM-dd'
+}
+
+// Initialize all input of date type.
+const calendars = bulmaCalendar.attach('[type="date"]', options);
+
+// Loop on each calendar initialized
+calendars.forEach(calendar => {
+	// Add listener to select event
+	calendar.on('select', date => {
+		console.log(date);
+	});
+});
+
+// To access to bulmaCalendar instance of an element
+const element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', datepicker => {
+		console.log(datepicker.data.value());
+	});
+}
